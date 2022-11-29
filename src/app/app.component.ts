51dcf53cf8de761abad7789aa0dca951926c2d83
mtruong68 <mary@pillandpillow.com>
2022-11-28 22:08:43 -0500

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,38 +8,27 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'donation';
-  windowRef = null;
 
   ngOnInit(){}
 
-  openForm(){
-    this.windowRef= window.open("/login","child", "toolbar=no,location=no,directories=no,status=no,menubar=no,titlebar=no,fullscreen=no,scrollbars=1,resizable=no,width=430,height=220,left=500,top=100");
-
-    this.windowRef.addEventListener("message",this.receivemessage.bind(this), false);
-  }
-  
-  receivemessage(evt:any){
-  console.log(evt.data)
-  }
-
-  selectedAmount: string = '';
   amounts: any = [
-    '$100',
-    '$50',
-    '$25',
-    '$15',
+    100,
+    50,
+    25,
+    15,
   ];
-  
+  selectedAmount: number = this.amounts[2];
+
   radioChangeHandler (event: any) {
     this.selectedAmount = event.target.value;
   }
 
-  selectedOccurence: string = '';
   occurences: any = [
     'ONE TIME',
     'MONTHLY'
   ];
-  
+  selectedOccurence: string = this.occurences[0];
+
   radioChangeHandler2 (event: any) {
     this.selectedOccurence = event.target.value;
   }
